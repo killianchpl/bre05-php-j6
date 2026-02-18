3cb0d7b3899d0011a238e3b5cabb4b383167f85f
Killian Chapelain <killian.chapelain@outlook.com>
2026-02-18 14:23:06 +0000

    exercice7

---
+++ edit-address.php
@@ -0,0 +1,24 @@
+<?php
+
+require 'connexion.php';
+
+$id      = $_POST['id'];
+$street  = $_POST['street'];
+$city    = $_POST['city'];
+$zipcode = $_POST['zipcode'];
+
+$query = $db->prepare('
+    UPDATE address
+    SET street = :street, city = :city, zipcode = :zipcode
+    WHERE id = :id
+');
+
+$parameters = [
+    'id'      => $id,
+    'street'  => $street,
+    'city'    => $city,
+    'zipcode' => $zipcode
+];
+
+$query->execute($parameters);
+?>
