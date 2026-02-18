1a837ec9bc0d5a10b3673ca0709428e6fd86a150
Killian Chapelain <killian.chapelain@outlook.com>
2026-02-18 13:32:09 +0000

    etape 6

---
+++ create-address.php
@@ -0,0 +1,24 @@
+<?php
+
+require 'connexion.php';
+
+$street  = $_POST['street'];
+$city    = $_POST['city'];
+$zipcode = $_POST['zipcode'];
+
+$query = $db->prepare('
+    INSERT INTO address (street, city, zipcode)
+    VALUES (:street, :city, :zipcode)
+');
+
+$parameters = [
+    'street'  => $street,
+    'city'    => $city,
+    'zipcode' => $zipcode
+];
+
+$query->execute($parameters);
+
+$id = $db->lastInsertId();
+
+echo "Adresse créée avec succès ! ID : " . $id;
